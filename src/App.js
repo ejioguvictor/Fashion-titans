05c314eba4c2926ce244910eae54ee1e8292f8c6
ejioguvictor <ejioguvictor6054@gmail.com>
2022-02-22 18:37:40 +0100

    updated components routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom'
 
 // pages
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
+import { useState } from 'react'
 
 function App() {
+  const [cartIsEmpty] = useState(true)
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -22,6 +25,15 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/products" element={<Products />} />
+          <Route path="test" element={(
+            <div>
+              <h2>Test Page</h2>
+              <p>Test</p>
+            </div>
+          )}
+          />
+          <Route Path="/redirect" element={<Navigate to="/about" />} />
+          <Route path="/checkout" element={cartIsEmpty ? <Navigate to="/products" /> : <p>Checkout</p>} />
         </Routes>
       </BrowserRouter>
     </div>
